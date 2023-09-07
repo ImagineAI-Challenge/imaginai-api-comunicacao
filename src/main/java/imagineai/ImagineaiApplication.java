22c3675d4149408e68d60d95f87e9e3c99b2ffc4
juniorcavicchioli <cavicchioli.adilson@gmail.com>
2023-09-06 22:43:34 -0300

    Adicionando estrutura básica do projeto

---
+++ src/main/java/imagineai/ImagineaiApplication.java
@@ -0,0 +1,13 @@
+package imagineai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagineaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagineaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imagineai/ImagineaiApplicationTests.java
@@ -0,0 +1,13 @@
+package imagineai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagineaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
